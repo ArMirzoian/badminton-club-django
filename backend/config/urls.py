3589e28b9c4b5b69aece037987902bf431c44a2b
ArMirzoian <artur.s.mirzoyan@gmail.com>
2026-03-23 17:03:23 +0300

    Add files via upload

---
+++ backend/config/urls.py
@@ -0,0 +1,28 @@
+"""
+URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # App URLs (создадим позже)
+    # path('', include('apps.games.urls')),
+    # path('accounts/', include('apps.accounts.urls')),
+    # path('registrations/', include('apps.registrations.urls')),
+]
+
+# Статика и медиа в development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    # Django Debug Toolbar
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
